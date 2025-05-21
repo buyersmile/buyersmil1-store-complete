50df526347632e407bdaceeb6d1b3c3f94877555
buyersmile <buyersmile.jj6@gmail.com>
2025-05-21 17:57:26 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-fetch('https://buyersmile-api.onrender.com/api/products')
+fetch('https://buyersmil1-api.onrender.com/api/products')
   .then(res => res.json())
   .then(data => {
     document.getElementById('products').innerHTML = data.map(p => `
@@ -11,7 +11,7 @@ fetch('https://buyersmile-api.onrender.com/api/products')
 document.getElementById('orderForm').addEventListener('submit', async e => {
   e.preventDefault();
   const form = new FormData(e.target);
-  const res = await fetch('https://buyersmile-api.onrender.com/api/order', {
+  const res = await fetch('https://buyersmil1-api.onrender.com/api/order', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({
